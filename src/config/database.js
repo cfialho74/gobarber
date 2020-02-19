0a721ba49e959ea89be908e509eab5c88f069581
Cristiano Fialho <cfialho74@hotmail.com>
2020-02-19 10:38:43 -0300

    Cadastro de Usuário

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'gobarber',
+  password: 'gobarber',
+  database: 'gobarber',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
