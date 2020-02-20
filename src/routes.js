03800e63ff817cff0a35e1f52729fad5002604a9
Cristiano Fialho <cfialho74@hotmail.com>
2020-02-20 18:00:37 -0300

    login-session-logout

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,21 @@ const express = require('express')
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 const routes = express.Router()
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
-routes.get('/signup', UserController.create)
+routes.get('/', guestMiddleware, SessionController.create)
+routes.post('/signin', SessionController.store)
+
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddleware)
+
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
+routes.get('/app/logout', SessionController.destroy)
 
 module.exports = routes
